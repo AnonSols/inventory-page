7585001defbd816ec9ab1fa91b9837a690b39293
Ubeath <soloschmail@gmail.com>
2024-08-24 00:12:22 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { DotBackgroundDemo } from "./ui/backgroundComponent/background";
+// import { SpotlightPreview } from "./ui/sportlightComponent/SpotlightPreview";
+
+const App = () => {
+  return (
+    <>
+      {/* <SpotlightPreview /> */}
+
+      <DotBackgroundDemo>Check</DotBackgroundDemo>
+    </>
+  );
+};
+
+export default App;
