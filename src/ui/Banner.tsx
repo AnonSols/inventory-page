141ee24cd62b12185508cc5d86472a83b3782e10
Ubeath <soloschmail@gmail.com>
2024-09-02 02:14:05 +0100

    minor changes

--- src/ui/Banner.tsx
+++ src/ui/Banner.tsx
@@ -8,7 +8,7 @@ type BannerType = {
 const Banner = ({ children, className }: BannerType) => {
   return (
     <div
-      className={`bg-gradient-to-r from-green-600 text-black to-green-500  dark:from-indigo-500 dark:to-purple-500 w-full flex justify-center  px-12 py-2 ${className}`}
+      className={`bg-gradient-to-r from-green-600 text-black to-green-500  dark:from-indigo-500 dark:to-purple-500 w-full flex justify-center  px-4 py-2 ${className}`}
     >
       {children}
     </div>
