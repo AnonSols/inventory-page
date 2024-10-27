d18f46e72ea554fd0118bf0c610d2d5e7efd6bc6
Ubeath <soloschmail@gmail.com>
2024-10-27 17:28:15 +0100

    minor changes

--- src/pages/PackageC.tsx
+++ src/pages/PackageC.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 // import Background from "../ui/Background";
 // import Nav from "../ui/Nav";
 import { Spotlight } from "../ui/sportlightComponent/Spotlight";
